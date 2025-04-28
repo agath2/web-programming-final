473e0180bb2e3e74628a24973749a21cc26d8aa9
helena-f <43306951+helena-f@users.noreply.github.com>
2025-04-28 11:55:10 -0400

    Stop tracking config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js - Store your API key here
-// IMPORTANT: Add this file to .gitignore to avoid exposing your API key publicly
-
-const apiKey = "AIzaSyC3PzlLp0ekgkplSYRuHZD_oHU5PnmlZRE"; // Replace with your actual API key
