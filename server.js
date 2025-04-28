d4ba4d32e31883c3a4b5feb342844101bc76926a
agath2 <agathayanghi@gmail.com>
2025-04-27 23:15:14 -0400

    feat: Connected signup form to mongo

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+const uri = 'mongodb+srv://agathayanghi:testTESTtest@clusterdemo.hfznyhl.mongodb.net/?retryWrites=true&w=majority&appName=Clusterdemo';
+const client = new MongoClient(uri);
+
+let db;
+
+// Connect to MongoDB Atlas
+async function connectDB() {
+    try {
+        await client.connect();
+        db = client.db('virtual_vet');
+        console.log('Connected to MongoDB Atlas');
+    } catch (error) {
+        console.error('MongoDB Atlas connection error:', error);
+    }
+}
+connectDB();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));  
+
+// Handle form submission
+app.post('/signup', async (req, res) => {
+        const { name, email, petName, petBreed, petAge } = req.body;
+    
+        try {
+            const signupCollection = db.collection('user_profile');
+            await signupCollection.insertOne({
+                name: name,
+                email: email,
+                petInfo: {
+                    name: petName,
+                    breed: petBreed,
+                    age: parseInt(petAge)
+                },
+                createdAt: new Date()
+            });
+            res.redirect('/signup_success.html');
+            //res.send('Signup successful!');
+        } catch (error) {
+            console.error('Error inserting signup:', error);
+            res.status(500).send('Error saving signup information.');
+        }
+    });
+    
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
